8d4f728900f933d9c471ef6b5b1388c50899e52d
Alejandro Rupérez Hernando <alexruperez@gmail.com>
2013-09-17 23:08:54 +0200

    Added Flurry, Google Analytics & New Relic libraries

--- src/com/alexruperez/alexruperez/AlexruperezActivity.java
+++ src/com/alexruperez/alexruperez/AlexruperezActivity.java
@@ -8,6 +8,10 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.view.KeyEvent;
 
+import com.google.analytics.tracking.android.EasyTracker;
+import com.flurry.android.FlurryAgent;
+import com.newrelic.agent.android.NewRelic;
+
 public class AlexruperezActivity extends Activity {
     /** Called when the activity is first created. */
     WebView mainWebView;
@@ -16,16 +20,33 @@ public class AlexruperezActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
+        NewRelic.withApplicationToken(
+                "AA83eb89f2f9a77a36d594773815891924ee9b6ab1"
+        ).start(this.getApplication());
+
         mainWebView = (WebView) findViewById(R.id.mainWebView);
-        
+
         WebSettings webSettings = mainWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        
+
         mainWebView.setWebViewClient(new AlexruperezWebViewClient());
         mainWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        
         mainWebView.loadUrl("http://alexruperez.com");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EasyTracker.getInstance(this).activityStart(this);
+        FlurryAgent.onStartSession(this, "PBPF647394365NNTBTFG");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EasyTracker.getInstance(this).activityStop(this);
+        FlurryAgent.onEndSession(this);
     }
 
     @Override
